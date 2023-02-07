55e2d22649f2d78461b2ce9df5b535724eca5c73
root <root@DESKTOP-P3GV7C6>
2023-02-07 15:53:30 +0800

    writing

--- cmd/api/biz/router/register.go
+++ cmd/api/biz/router/register.go
@@ -3,7 +3,7 @@
 package router
 
 import (
-	api "github.com/YANGJUNYAN0715/douyin/tree/guo/biz/router/api"
+	api "github.com/YANGJUNYAN0715/douyin/tree/guo/cmd/api/biz/router/api"
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
 
--- cmd/user/service/login_user.go
+++ cmd/user/service/login_user.go
@@ -1,5 +1,4 @@
 
-
 package service
 
 import (
