14996ac2bbc314c6edd306e520a8eccc25d5a834
root <root@DESKTOP-P3GV7C6>
2023-02-07 16:35:03 +0800

    writing

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/YANGJUNYAN0715/douyin/tree/guo/kitex_gen/user/userservice"
 	"github.com/YANGJUNYAN0715/douyin/tree/guo/pkg/consts"
-	// "github.com/YANGJUNYAN0715/douyin/tree/guo/pkg/rpcinfo"
+	"github.com/YANGJUNYAN0715/douyin/tree/guo/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	etcd "github.com/kitex-contrib/registry-etcd"
 )
@@ -17,7 +17,7 @@ func main() {
 		panic(err)
 	}
 	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8888")
-	svr := user.NewServer(new(UserServiceImpl),
+	svr := userservice.NewServer(new(UserServiceImpl),
 		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: consts.UserServiceName}), // server name
 		server.WithServiceAddr(addr), server.WithRegistry(r))
 
