cb9d3226a58eefaacdc1a2dffb3927d8000786a3
root <root@DESKTOP-P3GV7C6>
2023-02-23 11:12:46 +0800

    fix-feed

--- idl/api.thrift
+++ idl/api.thrift
@@ -189,12 +189,13 @@ struct MessageActionResponse {
 struct FeedRequest {
     1: i64 latest_time; // 可选参数，限制返回视频的最新投稿时间戳，精确到秒，不填表示当前时间
     2: string token; // 可选参数，登录用户设置
+    3: i64 user_id
 }
 
 // 例如当前请求的latest_time为9:00，那么返回的视频列表时间戳为[8:55,7:40, 6:30, 6:00]
 // 所有这些视频中，最早发布的是 6:00的视频，那么6:00作为下一次请求时的latest_time
 // 那么下次请求返回的视频时间戳就会小于6:00
-struct Feedresponse {
+struct FeedResponse {
     1: i32 status_code; // 状态码，0-成功，其他值-失败
     2: string status_msg; // 返回状态描述
     3: list<Video> video_list; // 视频列表
